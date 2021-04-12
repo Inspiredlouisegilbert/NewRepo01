c819a9efa032f1271b0168db4b391438d60d071f
lgilbert <lgilbert@ITLC3ML593.jhbdvt.co.za>
2021-04-12 15:13:02 +0200

    First Commit (ticket number 305)

---
+++ MyfirstRepo/src/main/java/com/org/MyfirstRepo/NewClassDemoget.java
@@ -0,0 +1,11 @@
+package com.org.MyfirstRepo;
+
+public class NewClassDemoget {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi IT health");
+
+	}
+
+}
